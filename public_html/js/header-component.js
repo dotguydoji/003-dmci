7b901481be5e646840fb6c7235227b870459f4b6
dotguydoji <thatguydoji@gmail.com>
2025-06-10 19:33:08 +0800

    update

--- public_html/js/header-component.js
+++ public_html/js/header-component.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const headerHTML = `
-        <header class="header">
+<header class="header">
             <nav class="nav-container">
                 <div class="logo">
                     <img src="/images/dmci-logo.png" alt="DMCI Homes Logo" />
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <li><a href="/index.html#home">Home</a></li>
                     <li><a href="/pages/browsemore.html">Locations</a></li>
                     <li><a href="/index.html#highlights">Highlights</a></li>
-                    <li><a href="https:
+                    <li><a href="https://sid.dmcihomes.com/OnlineCRF/Main.aspx?ac=NO35033" target="_blank">Register</a></li>
                     <li><a href="/index.html#contact">Contact Us</a></li>
                 </ul>
                 <!-- Mobile Menu Toggle -->
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     <form id="search" class="search-form">
                         <div class="container-input">
                             <input id="search-bar" type="text" placeholder="Location" name="text" class="input">
-                            <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http:
+                            <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fill-rule="evenodd"></path>
                             </svg>
                         </div>
